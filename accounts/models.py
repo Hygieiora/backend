fcd2b71ceca57d6c584a04148c32fcc7ac718c2b
Ike <66312028+ikeasamoahansah@users.noreply.github.com>
2024-03-29 19:52:50 +0000

    patient and doctor models

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,19 @@
 from django.db import models
-from django.contrib.auth.models import User
+from sp_models import BaseModel
 
 # Create your models here.
+class Patient(BaseModel):
+    school_email = models.CharField(max_length=256)
+    student_id = models.IntegerField(max_length=8)
+    programme = models.CharField(max_length=256)
+    year_of_study = models.IntegerField(max_length=1)
 
+    def __str__(self):
+        return f"{self.user.email} | {self.user.first_name} - {self.student_id}"
+
+
+class Doctor(BaseModel):
+    specialty = models.CharField(max_length=256)
+
+    def __str__(self):
+        return f"{self.user.email} | {self.specialty}"
---
+++ accounts/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Patient, Doctor
+
+class PatientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Patient
+        fields = '__all__'
+
+class DoctorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Doctor
+        fields = '__all__'
