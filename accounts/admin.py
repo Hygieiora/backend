29e365a792581ce495c49523206eccfd95d9f855
Ike <dylanraspino264@gmail.com>
2024-04-01 16:45:07 +0000

    Add models to admin site

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Doctor, Patient, CustomUser
 
 # Register your models here.
+admin.site.register(CustomUser)
+admin.site.register(Doctor)
+admin.site.register(Patient)
--- accounts/sp_models.py
+++ accounts/sp_models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class BaseModel(models.Model):
     date_of_birth = models.DateField(null=True, blank=True)
-    gender = models.CharField(max_length=1, choices=(('M','Male'), ('F', 'Female')), default='M')
+    gender = models.CharField(max_length=1, choices=(('M','Male'), ('F', 'Female')))
     
     class Meta:
         abstract = True
