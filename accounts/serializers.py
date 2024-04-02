30d737e41870c1847fe2b74ebf794c929656278c
Ike <dylanraspino264@gmail.com>
2024-04-02 09:50:53 +0000

    bug fix: token auth

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -20,7 +20,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'email', 'password', 'role')
-        kwargs = {
+        extra_kwargs = {
             'password': {'write_only': True}
         }
 
